9eff74e6cbd00a224e6d7d5157613379ff8b9a2a
Ghina Shofa <ghinashofa@Ghinas-Macbook.local>
2024-05-29 14:55:31 +0700

    update CSS

---
+++ script.js
@@ -0,0 +1,8 @@
+const apiKey = 'https://api.openweathermap.org/data/2.5/weather?lat={lat}&lon={lon}&appid={API key}';
+
+cel = tempCelcius;
+document.getElementById("temperature").addEventListener("click", ()=> {
+    if (cel !== undefined) {
+        let 
+    }
+})
