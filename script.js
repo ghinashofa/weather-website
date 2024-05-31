eb4c25c67ac5c6fc722039f186c15e8a2154f935
Ghina Shofa <ghinashofa@Ghinas-Macbook.local>
2024-05-31 12:19:55 +0700

    add date to each card & change the position of cards content using CSS

--- script.js
+++ script.js
@@ -23,13 +23,32 @@ async function fetchData(searchCity) { //mewakili pembuatan variabel
     const response7Days = await fetch(apiUrl7Days); //Permintaan HTTP ke API untuk mendapatkan data cuaca
     var data7Days = await response7Days.json();
     
-    //looping menampilkan weather selama 7 hari//
+    // ===== Menampilkan  Tanggal Pada Setiap Card ===== //
+    const dateString7Days = data7Days.daily.time; 
+    const options7Days = { 
+        weekday: 'long', 
+        day: '2-digit', 
+        month: 'long', 
+        year: 'numeric' 
+    }; 
+
+    const formatDate = (date) => date.toLocaleDateString('en-GB', options7Days);
+    let formattedDateCard = '';
+    dateString7Days.forEach(dateStr => {
+        const date = new Date(dateStr); 
+        const formattedDate = formatDate(date); 
+        formattedDateCard += `<p>${formattedDate}</p>`; 
+    });
+    
+
+    //looping menampilkan weather card selama 7 hari//
     for(let i=0; i<data7Days.daily.time.length; i++){
         document.getElementById("weatherDays").innerHTML += 
             `<div class="card-list">
+                <img src="images/icon-awan.png" width="100px" alt="">
                 <h3>Sunday</h3>
-                <img src="images/icon-awan.png" width="120px" style="padding-top: 20px;" alt="">
-                <p>${data7Days.daily.temperature_2m_max[i]}</p>
+                <p class="date-cards">${data7Days.daily.time[i]}</p>
+                <p class="temp">${data7Days.daily.temperature_2m_max[i]}°C</p>
             </div>`
 
     }
@@ -42,23 +61,46 @@ async function fetchData(searchCity) { //mewakili pembuatan variabel
     console.log(dataCurrent)
 
     document.getElementById('temperature').innerHTML = `${dataCurrent.current.temperature_2m} °C`;
+
+    // ====== menampilkan tanggal saat ini ======
+    const dateString = dataCurrent.current.time; 
+    const date = new Date(dateString); //konversi ke data object
+    const options = { 
+        weekday: 'long', 
+        day: '2-digit', 
+        month: 'long', 
+        year: 'numeric' 
+    }; //opsi untuk format date terbaru
+
+    const formattedDate = date.toLocaleDateString('en-GB', options); // Format the date
+    document.getElementById('currentDate').innerHTML = formattedDate;
 }
 
 fetchData();
 
-//membuat function search untuk memanggil inputan search//
+// === membuat function search untuk memanggil inputan search ===//
 
 function search() {
-    let searchCity = document.getElementById("search-input").value;
-    fetchData(searchCity);
-    kota(searchCity);
+    let searchCity = document.getElementById("search-input").value; //mengambil inputan yang dimasukkan oleh user
+    fetchData(searchCity); //diteruskan ke function search data untuk memproses data weather
+    kota(searchCity); //memanggil function kota untuk memberikan nilai input pengguna ke fungsi kota
 }
 
-function fetchData(city){
-    console.log(`fetching data for ${city}`);
+// ====== function untuk menampilkan nama kota sesuai search ======
+
+function fetchDataKota(city){
+    console.log(`fetching data for ${city}`); //mengambil data dari API untuk melanjutkan ke function kota
 }
 
 function kota(searchCity){
-    document.getElementById("nama-kota").innerHTML = searchCity;
+    document.getElementById("nama-kota").innerHTML = searchCity; //menampilkan inputan user di id nama-kota
 }
+
+
+
+
+
+
+
+
 
